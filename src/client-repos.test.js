1f7a396022ab73244994a1ad2a4190f2bb793624
Vlad Holubiev <golubev.vld@gmail.com>
2018-01-27 18:48:07 +0200

    test: cover compareBranches with tests

--- src/client-repos.test.js
+++ src/client-repos.test.js
@@ -3,18 +3,17 @@ jest.mock('./client');
 const {getClient} = require('./client');
 const {getRepoBranches, compareBranches} = require('./client-repos');
 
+const getBranchesMock = jest.fn().mockReturnValue({data: [{name: 'develop'}, {name: 'master'}]});
+const compareCommitsMock = jest.fn().mockReturnValue({data: []});
+
+getClient.mockReturnValue({
+  repos: {
+    getBranches: getBranchesMock,
+    compareCommits: compareCommitsMock
+  }
+});
+
 describe('#getRepoBranches', () => {
-  const getBranchesMock = jest.fn();
-  getBranchesMock.mockReturnValue({
-    data: [{name: 'develop'}, {name: 'master'}]
-  });
-
-  getClient.mockReturnValue({
-    repos: {
-      getBranches: getBranchesMock
-    }
-  });
-
   beforeEach(() => {
     getBranchesMock.mockClear();
   });
@@ -31,5 +30,30 @@ describe('#getRepoBranches', () => {
   it('should return 2 branches', async () => {
     const branches = await getRepoBranches({org: 'my-org', repo: 'my-repo'});
     expect(branches).toEqual(['develop', 'master']);
+  });
+});
+
+describe('#compareBranches', () => {
+  beforeEach(() => {
+    compareCommitsMock.mockClear();
+  });
+
+  it('should export compareBranches function', () => {
+    expect(compareBranches).toBeInstanceOf(Function);
+  });
+
+  it('should call compareCommits w/ develop and master', async () => {
+    await compareBranches({org: 'my-org', repo: 'my-repo'});
+    expect(compareCommitsMock).toBeCalledWith({
+      base: 'master',
+      head: 'develop',
+      owner: 'my-org',
+      repo: 'my-repo'
+    });
+  });
+
+  it('should return array of changes', async () => {
+    const changes = await compareBranches({org: 'my-org', repo: 'my-repo'});
+    expect(changes).toEqual([]);
   });
 });
