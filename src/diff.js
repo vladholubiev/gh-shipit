bf4394f2d928d35c9eccb816027303486122e5b8
Vlad Holubiev <golubev.vld@gmail.com>
2018-01-27 19:55:46 +0200

    feat: add progress tracking for promise loading all repos

--- src/diff.js
+++ src/diff.js
@@ -2,28 +2,47 @@ const _ = require('lodash');
 const fp = require('lodash/fp');
 const {relativeTime} = require('human-date');
 const longest = require('longest');
+const PProgress = require('p-progress');
 const {getBranchDiff} = require('./repos');
 
 module.exports.getReposBranchesDiff = async function({org, repos}) {
-  const diffs = await Promise.all(repos.map(repo => getBranchDiff({org, repo})));
-  const diffsFormatted = fp.flow(
+  const diffs = await getAllReposDiffs({org, repos});
+  return fp.flow(sortDiffs, padDiffs)(diffs);
+};
+
+function getAllReposDiffs({org, repos}) {
+  return Promise.all(
+    repos.map((repo, i) =>
+      PProgress.fn(async (repo, i, progress) => {
+        const resp = await getBranchDiff({org, repo});
+
+        progress(i / repos.length);
+
+        return resp;
+      })(repo, i)
+    )
+  );
+}
+
+function sortDiffs(diffs) {
+  return fp.flow(
     fp.reject({status: 'no-branch'}),
     fp.orderBy([d => new Date(d.lastCommitDate)], ['desc']),
     fp.map(d => _.set(d, 'lastCommitDateFormatted', formatDate(d.lastCommitDate)))
   )(diffs);
+}
 
-  const widestDateLength = fp.flow(fp.map('lastCommitDateFormatted'), longest, fp.size)(
-    diffsFormatted
-  );
+function padDiffs(diff) {
+  const widestDateLength = fp.flow(fp.map('lastCommitDateFormatted'), longest, fp.size)(diff);
 
-  return _.map(diffsFormatted, d => {
+  return _.map(diff, d => {
     _.set(d, 'lastCommitDateFormatted', _.padStart(d.lastCommitDateFormatted, widestDateLength));
     _.set(d, 'ahead_by', _.padEnd(`+${d.ahead_by}`, 4));
     _.set(d, 'behind_by', _.padStart(`-${d.behind_by}`, 4));
 
     return d;
   });
-};
+}
 
 function formatDate(date) {
   return relativeTime(new Date(date));
