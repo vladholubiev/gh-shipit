160fdc709d6a42f15bb58f6139ab1b5401dea7b3
Vlad Holubiev <golubev.vld@gmail.com>
2018-01-28 00:54:03 +0200

    feat: print error from github api

--- src/cli.js
+++ src/cli.js
@@ -8,22 +8,28 @@ const {createReleaseNotes} = require('./client-releases');
 
 (async () => {
   const org = await askOrg();
-  const repo = await askRepo(org);
+  const repo = await askRepo(org); // todo show latest release
   const action = await askRepoAction();
 
   if (action === 'prepare-release') {
-    const version = await askVersion({org, repo});
-    const commitHash = await getLastDevelopCommitSHA({org, repo});
+    try {
+      const version = await askVersion({org, repo});
+      const commitHash = await getLastDevelopCommitSHA({org, repo});
 
-    await createReleaseBranch({org, repo, version, commitHash});
-    console.log(logSymbols.success, `Created branch release/v${version}!`);
+      await createReleaseBranch({org, repo, version, commitHash});
+      console.log(logSymbols.success, `Created branch release/v${version}!`);
 
-    const releaseTitle = await askReleaseTitle({org, repo});
+      const releaseTitle = await askReleaseTitle({org, repo});
 
-    await createReleaseNotes({org, repo, version, releaseTitle});
-    console.log(logSymbols.success, `Created Release Notes!`);
+      await createReleaseNotes({org, repo, version, releaseTitle});
+      console.log(logSymbols.success, `Created Release Notes!`);
 
-    await createReleasePR({org, repo, version, releaseTitle});
-    console.log(logSymbols.success, `Created Pull Request!`);
+      await createReleasePR({org, repo, version, releaseTitle});
+      console.log(logSymbols.success, `Created Pull Request!`);
+
+      // todo open in browser
+    } catch (error) {
+      console.log(logSymbols.error, JSON.parse(error.message).message);
+    }
   }
 })();
