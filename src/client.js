2c36c0338971a47057497a5a2b350a1fb0a42067
Vlad Holubiev <golubev.vld@gmail.com>
2018-01-28 04:24:57 +0200

    chore: setup graphql github client

--- src/client.js
+++ src/client.js
@@ -1,6 +1,11 @@
 const octokit = require('@octokit/rest')();
+const {ApolloClient} = require('apollo-client');
+const {HttpLink} = require('apollo-link-http');
+const {InMemoryCache} = require('apollo-cache-inmemory');
+const {setContext} = require('apollo-link-context');
 
 let client;
+let clientGQL;
 
 module.exports.getClient = function() {
   if (client) {
@@ -15,4 +20,27 @@ module.exports.getClient = function() {
   client = octokit;
 
   return client;
+};
+
+module.exports.getClientGraphQL = function() {
+  if (clientGQL) {
+    return clientGQL;
+  }
+
+  const authLink = setContext((_, {headers}) => {
+    const token = process.env.GITHUB_TOKEN;
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `token ${token}` : null
+      }
+    };
+  });
+
+  clientGQL = new ApolloClient({
+    link: authLink.concat(new HttpLink({uri: 'https://api.github.com/graphql'})),
+    cache: new InMemoryCache()
+  });
+
+  return clientGQL;
 };
