b58f6b705bc14363b34921adb2452ed719be1f90
Vlad Holubiev <golubev.vld@gmail.com>
2018-01-28 02:03:43 +0200

    perf: use p-map with 24 concurrency to improve perf on big orgs

--- src/diff.js
+++ src/diff.js
@@ -1,17 +1,19 @@
-const PProgress = require('p-progress');
+const pProgress = require('p-progress');
+const pMap = require('p-map');
 const {getBranchDiff} = require('./repos');
 
 module.exports.getAllReposDiffs = function({org, repos}) {
-  return PProgress.fn((repos, progress) => {
+  return pProgress.fn((repos, progress) => {
     let i = 0;
 
-    return Promise.all(
-      repos.map(repo =>
+    return pMap(
+      repos,
+      repo =>
         getBranchDiff({org, repo}).then(resp => {
           progress(i++ / repos.length);
           return resp;
-        })
-      )
+        }),
+      {concurrency: 24}
     );
   })(repos);
 };
