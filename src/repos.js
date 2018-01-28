7e7a82f3c956ce5117d16034769e01882cd568e6
Vlad Holubiev <golubev.vld@gmail.com>
2018-01-28 02:59:12 +0200

    feat: skip diff for recently merged master -> develop PRs

--- src/repos.js
+++ src/repos.js
@@ -11,6 +11,13 @@ module.exports.getBranchDiff = async function({org, repo}) {
   const {status, ahead_by, behind_by, commits, base_commit} = await compareBranches({org, repo});
   const lastRelease = await getLastRelease({org, repo});
 
+  const lastHeadCommitAuthor = _.get(commits.reverse(), '[0].commit.committer.name', '');
+
+  const isRecentlyMergedMasterDevelopSync = lastHeadCommitAuthor === 'GitHub' && ahead_by === 1;
+  if (isRecentlyMergedMasterDevelopSync) {
+    return {org, repo, status: 'no-branch'};
+  }
+
   const lastHeadCommitDate = _.get(commits.reverse(), '[0].commit.author.date', '');
   const lastBaseCommitDate = _.get(base_commit, 'commit.author.date', '');
   const lastCommitDate = lastHeadCommitDate || lastBaseCommitDate;
