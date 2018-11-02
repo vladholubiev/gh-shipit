30f8e4224628171ca285fc9917a13c3d435e06ef
Vlad Holubiev <golubev.vld@gmail.com>
2018-11-02 17:15:27 +0200

    feat: support CSV output for latest releases

--- src/flows/view-releases.js
+++ src/flows/view-releases.js
@@ -1,6 +1,14 @@
+const CSVSerializer = require('json2csv').Parser;
 const {getOrgReleases} = require('../client-releases');
 const {getLatestReleases} = require('../helpers-releases');
 const {printReleasesTable} = require('../releases-table');
+
+const fields = [
+  'repo',
+  {label: 'date', value: row => row.date.toLocaleDateString()},
+  'version',
+  'name'
+];
 
 module.exports.viewReleases = async function({org, format}) {
   const releases = await getOrgReleases(org);
@@ -8,5 +16,10 @@ module.exports.viewReleases = async function({org, format}) {
 
   if (format === 'table') {
     return printReleasesTable(latestReleases);
+  }
+
+  if (format === 'csv') {
+    const serializer = new CSVSerializer({fields});
+    console.log(serializer.parse(latestReleases));
   }
 };
