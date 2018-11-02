810ee9e58f9d135479dacfa11e038dc34ed49149
Vlad Holubiev <golubev.vld@gmail.com>
2018-11-02 16:50:27 +0200

    feat: don't print releases histogram

--- src/flows/view-releases.js
+++ src/flows/view-releases.js
@@ -1,13 +1,11 @@
 const {getOrgReleases} = require('../client-releases');
 const {getLatestReleases} = require('../helpers-releases');
 const {printReleasesTableInRange} = require('../releases-table');
-const {printReleasesHistogram} = require('../releases-histogram');
 
 module.exports.viewReleases = async function(org) {
   const releases = await getOrgReleases(org);
   const latestReleases = getLatestReleases(releases);
 
-  printReleasesHistogram(latestReleases);
   printReleasesTableInRange(latestReleases, 'thisWeek');
   printReleasesTableInRange(latestReleases, 'lastWeek');
   printReleasesTableInRange(latestReleases, 'thisMonth');
--- src/flows/view-releases.test.js
+++ src/flows/view-releases.test.js
@@ -1,12 +1,10 @@
 jest.mock('../client-releases');
 jest.mock('../helpers-releases');
 jest.mock('../releases-table');
-jest.mock('../releases-histogram');
 
 const {getOrgReleases} = require('../client-releases');
 const {getLatestReleases} = require('../helpers-releases');
 const {printReleasesTableInRange} = require('../releases-table');
-const {printReleasesHistogram} = require('../releases-histogram');
 const {viewReleases} = require('./view-releases');
 
 const latestReleasesMock = [{a: 1}];
@@ -25,11 +23,6 @@ it('should call getOrgReleases w/ org name', async () => {
 it('should call getLatestReleases w/ releases response', async () => {
   await viewReleases('my-org');
   expect(getLatestReleases).toBeCalledWith(latestReleasesMock);
-});
-
-it('should call printReleasesHistogram w/ releases response', async () => {
-  await viewReleases('my-org');
-  expect(printReleasesHistogram).toBeCalledWith(latestReleasesMock);
 });
 
 it('should call printReleasesTableInRange w/ releases response for this week', async () => {
